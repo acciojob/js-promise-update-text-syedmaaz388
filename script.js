b7c4b28c8d56bd9b6a8ba1b005f15587a24a01e4
syedmaaz388 <63224539+syedmaaz388@users.noreply.github.com>
2023-04-14 21:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+sayHello().then((message) => {
+  document.getElementById("output").textContent = message;
+});
